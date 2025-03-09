7ba1f8d73b1df5f5fa4db6ecd261d92be02b661b
LOKESHGOPAL21 <125382921+LOKESHGOPAL21@users.noreply.github.com>
2025-03-09 22:37:20 +0530

    Time: 5 ms (61.17%), Space: 63.2 MB (37.43%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int n=colors.length;
+        int count=0;
+        int l=0;
+        int limit=n+k-1;
+
+        while(l<n){
+            //Find largest valid window
+            int r=l+1;
+            while(r<limit && colors[(r-1)%n]!=colors[r%n]){
+                 r++;
+            }
+               
+            
+            //Count Valid Windows
+            if(r-l >= k)
+                count += (r-l) - k + 1;
+            
+            //Set new start point
+            l=r;
+        }
+        return count;
+    }
+}
