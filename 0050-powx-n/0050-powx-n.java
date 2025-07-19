79cea241ddb52c9d3cdf8bd4d2eba54a16cce264
LOKESHGOPAL21 <125382921+LOKESHGOPAL21@users.noreply.github.com>
2025-07-19 08:01:01 +0530

    Time: 0 ms (100%), Space: 43.4 MB (10.25%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x,int n){
+        return pow(x,(long)n);
+    }
+    public double pow(double x, long n) {
+        if(n==0){
+            return 1.0; 
+        }
+        if(n<0) return 1/pow(x,-n);
+
+        double half = pow(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }
+            return half*half*x;
+    }
+}
